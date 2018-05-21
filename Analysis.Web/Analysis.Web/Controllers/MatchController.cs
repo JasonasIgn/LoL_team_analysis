806461c88da55cb924b000ed47b05b94d15edbed
JasonasIgn <ignas.jasonas@gmail.com>
2018-05-21 23:22:58 +0300

    Add match controller

---
+++ Analysis.Web/Analysis.Web/Controllers/MatchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace Analysis.Web.Controllers
+{
+    [Route("/[controller]")]
+    public class MatchController : Controller
+    {
+
+        public MatchController()
+        {
+
+        }
+
+    }
+}
