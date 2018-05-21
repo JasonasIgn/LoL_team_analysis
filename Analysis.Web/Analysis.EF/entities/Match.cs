b0eb43c40e1b3a315f94603f8d2e534bc95646ec
JasonasIgn <ignas.jasonas@gmail.com>
2018-05-21 23:26:29 +0300

    Add match entity class

---
+++ Analysis.Web/Analysis.EF/entities/Match.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Analysis.EF.entities
+{
+    public class Match
+    {
+        public int top1ChampionId { get; set; }
+        public int jgl1ChampionId { get; set; }
+        public int mid1ChampionId { get; set; }
+        public int bot1ChampionId { get; set; }
+        public int sup1ChampionId { get; set; }
+        public int top2ChampionId { get; set; }
+        public int jgl2ChampionId { get; set; }
+        public int mid2ChampionId { get; set; }
+        public int bot2ChampionId { get; set; }
+        public int sup2ChampionId { get; set; }
+
+    }
+}
