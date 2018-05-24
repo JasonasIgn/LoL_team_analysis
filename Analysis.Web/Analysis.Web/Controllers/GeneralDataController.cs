b4bd73c41eb83851945c1e1fd9ccaf3919778c01
JasonasIgn <ignas.jasonas@gmail.com>
2018-05-24 12:17:15 +0300

    Add general data controller

---
+++ Analysis.Web/Analysis.Web/Controllers/GeneralDataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Analysis.Application.main;
+using Analysis.Application.main.generalData;
+using Analysis.Application.main.match;
+using Analysis.EF.entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace Analysis.Web.Controllers
+{
+    [Route("/[controller]")]
+    public class GeneralDataController : Controller
+    {
+        protected readonly IGeneralDataService _generalDataService;
+        public GeneralDataController(IGeneralDataService generalDataService)
+        {
+            _generalDataService = generalDataService;
+        }
+
+
+       // [HttpGet("{id}")]
+        //public GeneralData Get(int id)
+       // {
+            //return _generalDataService.SaveRiotMatchById(id, api);
+       // }
+    }
+}
