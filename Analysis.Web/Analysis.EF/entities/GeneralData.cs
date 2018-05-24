7b5e693a2bc960d36fcc84c6dcfe0b4a0fe8a8b7
JasonasIgn <ignas.jasonas@gmail.com>
2018-05-24 12:02:14 +0300

    Add general data class to backend

---
+++ Analysis.Web/Analysis.EF/entities/GeneralData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Analysis.EF.entities
+{
+    class GeneralData
+    {
+        public int Id { get; set; }
+        public string CurrentMatchId { get; set; }
+        public int TotalTeamCombinantions { get; set; }
+    }
+}
