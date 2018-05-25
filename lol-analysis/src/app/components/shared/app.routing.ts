9ce6c6b55d7038293b27f2d5584fcecb3bcc70e1
JasonasIgn <ignas.jasonas@gmail.com>
2018-05-25 14:36:54 +0300

    Route seach teams module

--- lol-analysis/src/app/components/shared/app.routing.ts
+++ lol-analysis/src/app/components/shared/app.routing.ts
@@ -3,12 +3,14 @@ import { RouterModule } from '@angular/router';
 
 import { CollectDataComponent } from '../collect-data/collect-data.component';
 import { AppComponent } from '../../app.component';
+import { SearchTeamsComponent } from '../search-teams/search-teams.component';
 
 @NgModule({
   imports: [
       RouterModule.forRoot([
           //{ path: '' , component: AppComponent},
           { path: 'collect-data' , component: CollectDataComponent},
+          { path: 'search-teams' , component: SearchTeamsComponent},
           // otherwise redirect to home
           { path: '**', redirectTo: '' }
       ])
