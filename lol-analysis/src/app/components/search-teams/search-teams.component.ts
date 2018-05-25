7a534b6fc9e621c0f321625a319f2c5ebb970aec
JasonasIgn <ignas.jasonas@gmail.com>
2018-05-25 17:43:34 +0300

    Implemented team input

--- lol-analysis/src/app/components/search-teams/search-teams.component.ts
+++ lol-analysis/src/app/components/search-teams/search-teams.component.ts
@@ -10,10 +10,20 @@ import { ChampionModel } from '../../models/champion.model';
 export class SearchTeamsComponent implements OnInit {
 
   championList: ChampionModel[];
+  searchChampions: ChampionModel[] = new Array<ChampionModel>(10);
+  selected: number = -1;
 
   constructor(private championService: ChampionService) { }
 
   ngOnInit() {
+    for (var i = 0; i < 10; i++)
+    {
+      this.searchChampions[i] = new ChampionModel();
+        this.searchChampions[i].id = -1;
+        this.searchChampions[i].title = "not selected";
+        this.searchChampions[i].name = "not selected";
+        this.searchChampions[i].key = "not selected";
+    }
       this.championService.getAllChampions().subscribe((data: ChampionModel[]) =>{
         this.championList = data;
       });
@@ -23,5 +33,24 @@ export class SearchTeamsComponent implements OnInit {
     this.championService.updateChampions(api).subscribe((data: ChampionModel[]) =>{
       console.log("success");
     });
+  }
+  setSelected(nr: number)
+  {
+    this.selected = nr;
+    console.log(this.selected);
+  }
+  pickChampion(id:number)
+  {
+    if (this.selected != -1)
+    {
+      for (var i = 0; i < this.championList.length; i++)
+      {
+        if (this.championList[i].id == id)
+        {
+          this.searchChampions[this.selected] = this.championList[i];
+        }
+        
+      }
+    }
   }
 }
