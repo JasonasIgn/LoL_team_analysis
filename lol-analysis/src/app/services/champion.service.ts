9cdef8bfdb92f47e64a50e87f7258df753004c7c
JasonasIgn <ignas.jasonas@gmail.com>
2018-05-25 15:27:06 +0300

    Add champion service to frontend

---
+++ lol-analysis/src/app/services/champion.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+import { GeneralData } from '../models/generaldata.model';
+import { ChampionModel } from '../models/champion.model';
+
+@Injectable()
+export class GeneralDataService {
+
+  constructor(private http: Http) { }
+    url = "http://localhost:1756/Champion";
+    getChampions(): Observable<ChampionModel[]> {
+        return this.http.get(this.url)
+        .map((res: Response) => res.json() as ChampionModel[])
+        .catch((error: any) => Observable.throw(error)); 
+    }
+}
