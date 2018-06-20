1e9eb2bf1bfbdca62ada46813bc64ea2ab6ccf6e
JasonasIgn <ignas.jasonas@gmail.com>
2018-06-20 21:43:24 +0300

    Remove deleted components imports

--- lol-analysis/src/app/components/shared/app.routing.ts
+++ lol-analysis/src/app/components/shared/app.routing.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { CollectDataComponent } from '../collect-data/collect-data.component';
-import { AppComponent } from '../../app.component';
 import { CounterComponent } from '../counter/counter.component';
-import { AdminComponent } from '../admin/admin.component';
 import { SynergyComponent } from '../synergy/synergy.component';
 import { HomeComponent } from '../home/home.component';
 import { ContactComponent } from '../contact/contact.component';
