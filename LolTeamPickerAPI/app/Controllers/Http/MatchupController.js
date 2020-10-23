53fb718196674de58bee77192fb3d72012e3566c
ignjas <ignas.jasonas@gmail.com>
2020-10-23 23:31:59 +0300

    Add new format for storing big data

--- LolTeamPickerAPI/app/Controllers/Http/MatchupController.js
+++ LolTeamPickerAPI/app/Controllers/Http/MatchupController.js
@@ -1,12 +1,14 @@
 "use strict";
 
+const { match } = require("@adonisjs/framework/src/Route/Manager");
+
 const Axios = use("axios");
-const Matchup = use("App/Models/Matchup");
 const Server = use("App/Models/Server");
 const Player = use("App/Models/Player");
 const CrawledGame = use("App/Models/CrawledGame");
 const crawlHelpers = use("App/Helpers/crawlHelpers");
 const roleIdentification = use("App/Helpers/roleIdentification");
+const storing = use("App/Helpers/storingHelpers");
 const Config = use("App/Models/Config");
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
@@ -77,121 +79,76 @@ class MatchupController {
         return;
       }
 
-      playerMatchlistResponse.data.matches.forEach(async (match) => {
-        const matchId = match.gameId;
-        const hasBeenCraweled = await CrawledGame.findBy("gameId", matchId);
-        if (Boolean(hasBeenCraweled)) return;
+      const promises = playerMatchlistResponse.data.matches.map(
+        async (match) => {
+          const matchId = match.gameId;
+          const hasBeenCraweled = await CrawledGame.findBy("gameId", matchId);
+          if (Boolean(hasBeenCraweled)) return;
 
-        await server.crawledGames().create({ gameId: matchId });
-        const gameResponse = await Axios.get(
-          `https://${server.name}.api.riotgames.com//lol/match/v4/matches/${matchId}`,
-          {
-            headers: {
-              "X-Riot-Token": process.env.LOL_API_KEY,
-            },
-          }
-        );
-        let matchupData = {
-          team1_top: -1,
-          team1_jungle: -1,
-          team1_mid: -1,
-          team1_adc: -1,
-          team1_support: -1,
-          team2_top: -1,
-          team2_jungle: -1,
-          team2_mid: -1,
-          team2_adc: -1,
-          team2_support: -1,
-          team1_wins: 0,
-          team2_wins: 0,
-        };
-        const { data } = gameResponse;
-        let team1Champions = [];
-        let team2Champions = [];
-        const team1Id = data.teams[0].teamId;
-        const team1Win = data.teams[0].win === "Win";
-        const team2Id = data.teams[1].teamId;
-        const team2Win = data.teams[1].win === "Win";
-        matchupData.team1_wins = team1Win ? 1 : 0;
-        matchupData.team2_wins = team2Win ? 1 : 0;
-
-        data.participantIdentities.forEach(async (identity) => {
-          const playerSummonerName = identity.player.summonerName;
-          const alreadyAdded = await Player.findBy(
-            "summoner_name",
-            playerSummonerName
+          await server.crawledGames().create({ gameId: matchId });
+          const gameResponse = await Axios.get(
+            `https://${server.name}.api.riotgames.com//lol/match/v4/matches/${matchId}`,
+            {
+              headers: {
+                "X-Riot-Token": process.env.LOL_API_KEY,
+              },
+            }
           );
-          if (Boolean(alreadyAdded)) return;
-          await server.players().create({ summoner_name: playerSummonerName });
-        });
+          let matchupData = {};
+          const { data } = gameResponse;
+          let team1Champions = [];
+          let team2Champions = [];
+          const team1Id = data.teams[0].teamId;
+          const team1Win = data.teams[0].win === "Win";
+          const team2Id = data.teams[1].teamId;
+          const team2Win = data.teams[1].win === "Win";
+          matchupData.team1_wins = team1Win ? 1 : 0;
+          matchupData.team2_wins = team2Win ? 1 : 0;
 
-        data.participants.forEach((participant) => {
-          if (participant.teamId === team1Id) {
-            team1Champions.push(participant.championId);
-          } else {
-            team2Champions.push(participant.championId);
-          }
-        });
-        const team1Roles = roleIdentification.getRoles(
-          championRoles,
-          team1Champions
-        );
-        const team2Roles = roleIdentification.getRoles(
-          championRoles,
-          team2Champions
-        );
-
-        matchupData.team1_top = team1Roles.TOP;
-        matchupData.team1_jungle = team1Roles.JUNGLE;
-        matchupData.team1_mid = team1Roles.MIDDLE;
-        matchupData.team1_adc = team1Roles.BOTTOM;
-        matchupData.team1_support = team1Roles.UTILITY;
-        matchupData.team2_top = team2Roles.TOP;
-        matchupData.team2_jungle = team2Roles.JUNGLE;
-        matchupData.team2_mid = team2Roles.MIDDLE;
-        matchupData.team2_adc = team2Roles.BOTTOM;
-        matchupData.team2_support = team2Roles.UTILITY;
-
-        const existingMatch = await Matchup.findBy({
-          team1_top: matchupData.team1_top,
-          team1_jungle: matchupData.team1_jungle,
-          team1_mid: matchupData.team1_mid,
-          team1_adc: matchupData.team1_adc,
-          team1_support: matchupData.team1_support,
-          team2_top: matchupData.team2_top,
-          team2_jungle: matchupData.team2_jungle,
-          team2_mid: matchupData.team2_mid,
-          team2_adc: matchupData.team2_adc,
-          team2_support: matchupData.team2_support,
-        });
-
-        if (existingMatch) {
-          existingMatch.team1_wins += matchupData.team1_wins;
-          existingMatch.team2_wins += matchupData.team2_wins;
-          await existingMatch.save();
-        } else {
-          const existingMatch2 = await Matchup.findBy({
-            team2_top: matchupData.team1_top,
-            team2_jungle: matchupData.team1_jungle,
-            team2_mid: matchupData.team1_mid,
-            team2_adc: matchupData.team1_adc,
-            team2_support: matchupData.team1_support,
-            team1_top: matchupData.team2_top,
-            team1_jungle: matchupData.team2_jungle,
-            team1_mid: matchupData.team2_mid,
-            team1_adc: matchupData.team2_adc,
-            team1_support: matchupData.team2_support,
+          const promises = data.participantIdentities.map(async (identity) => {
+            const playerSummonerName = identity.player.summonerName;
+            const alreadyAdded = await Player.findBy(
+              "summoner_name",
+              playerSummonerName
+            );
+            if (Boolean(alreadyAdded)) return;
+            await server
+              .players()
+              .create({ summoner_name: playerSummonerName });
           });
-          if (existingMatch2) {
-            existingMatch2.team1_wins += matchupData.team2_wins;
-            existingMatch2.team2_wins += matchupData.team1_wins;
-            await existingMatch2.save();
-          } else {
-            await Matchup.create(matchupData);
-          }
-        }
-      });
+          await Promise.all(promises);
 
+          data.participants.forEach((participant) => {
+            if (participant.teamId === team1Id) {
+              team1Champions.push(participant.championId);
+            } else {
+              team2Champions.push(participant.championId);
+            }
+          });
+          const team1Roles = roleIdentification.getRoles(
+            championRoles,
+            team1Champions
+          );
+          const team2Roles = roleIdentification.getRoles(
+            championRoles,
+            team2Champions
+          );
+
+          matchupData.team1_top = team1Roles.TOP;
+          matchupData.team1_jungle = team1Roles.JUNGLE;
+          matchupData.team1_mid = team1Roles.MIDDLE;
+          matchupData.team1_adc = team1Roles.BOTTOM;
+          matchupData.team1_support = team1Roles.UTILITY;
+          matchupData.team2_top = team2Roles.TOP;
+          matchupData.team2_jungle = team2Roles.JUNGLE;
+          matchupData.team2_mid = team2Roles.MIDDLE;
+          matchupData.team2_adc = team2Roles.BOTTOM;
+          matchupData.team2_support = team2Roles.UTILITY;
+
+          await storing.storeMatchup(matchupData);
+        }
+      );
+      await Promise.all(promises);
       response.status(200).send({});
     } catch (e) {
       console.log(e);
---
+++ LolTeamPickerAPI/app/Helpers/storingHelpers.js
@@ -0,0 +1,289 @@
+"use strict";
+
+const Matchup = use("App/Models/Matchup");
+
+async function storeMatch(matchupData) {
+  try {
+    const existingMatch = await Matchup.findBy({
+      team1_top: matchupData.team1_top,
+      team1_jungle: matchupData.team1_jungle,
+      team1_mid: matchupData.team1_mid,
+      team1_adc: matchupData.team1_adc,
+      team1_support: matchupData.team1_support,
+      team2_top: matchupData.team2_top,
+      team2_jungle: matchupData.team2_jungle,
+      team2_mid: matchupData.team2_mid,
+      team2_adc: matchupData.team2_adc,
+      team2_support: matchupData.team2_support,
+    });
+
+    if (existingMatch) {
+      existingMatch.team1_wins += matchupData.team1_wins;
+      existingMatch.team2_wins += matchupData.team2_wins;
+      await existingMatch.save();
+    } else {
+      const existingMatch2 = await Matchup.findBy({
+        team2_top: matchupData.team1_top,
+        team2_jungle: matchupData.team1_jungle,
+        team2_mid: matchupData.team1_mid,
+        team2_adc: matchupData.team1_adc,
+        team2_support: matchupData.team1_support,
+        team1_top: matchupData.team2_top,
+        team1_jungle: matchupData.team2_jungle,
+        team1_mid: matchupData.team2_mid,
+        team1_adc: matchupData.team2_adc,
+        team1_support: matchupData.team2_support,
+      });
+      if (existingMatch2) {
+        existingMatch2.team1_wins += matchupData.team2_wins;
+        existingMatch2.team2_wins += matchupData.team1_wins;
+        await existingMatch2.save();
+      } else {
+        await Matchup.create(matchupData);
+      }
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+function getResetMatchupData(matchupData, resetPositionsArray) {
+  const newMatchupData = { ...matchupData };
+  if (resetPositionsArray[0]) {
+    newMatchupData.team1_top = -1;
+  }
+  if (resetPositionsArray[1]) {
+    newMatchupData.team1_jungle = -1;
+  }
+  if (resetPositionsArray[2]) {
+    newMatchupData.team1_mid = -1;
+  }
+  if (resetPositionsArray[3]) {
+    newMatchupData.team1_adc = -1;
+  }
+  if (resetPositionsArray[4]) {
+    newMatchupData.team1_support = -1;
+  }
+  if (resetPositionsArray[5]) {
+    newMatchupData.team2_top = -1;
+  }
+  if (resetPositionsArray[6]) {
+    newMatchupData.team2_jungle = -1;
+  }
+  if (resetPositionsArray[7]) {
+    newMatchupData.team2_mid = -1;
+  }
+  if (resetPositionsArray[8]) {
+    newMatchupData.team2_adc = -1;
+  }
+  if (resetPositionsArray[9]) {
+    newMatchupData.team2_support = -1;
+  }
+  return newMatchupData;
+}
+
+async function storeSingleCombinationMatchups(matchupData) {
+  try {
+    for (let i = 0; i < 10; i++) {
+      const positions = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+      positions[i] = 0;
+      const alteredMatchupData = getResetMatchupData(matchupData, positions);
+      await storeMatch(alteredMatchupData);
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storeDoubleCombinationMatchups(matchupData) {
+  try {
+    for (let i = 0; i < 9; i++) {
+      for (let j = i + 1; j < 10; j++) {
+        const positions = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+        positions[i] = 0;
+        positions[j] = 0;
+        const alteredMatchupData = getResetMatchupData(matchupData, positions);
+        await storeMatch(alteredMatchupData);
+      }
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storeTripleCombinationMatchups(matchupData) {
+  try {
+    for (let i = 0; i < 8; i++) {
+      for (let j = i + 1; j < 9; j++) {
+        for (let k = j + 1; k < 10; k++) {
+          const positions = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+          positions[i] = 0;
+          positions[j] = 0;
+          positions[k] = 0;
+          const alteredMatchupData = getResetMatchupData(
+            matchupData,
+            positions
+          );
+          await storeMatch(alteredMatchupData);
+        }
+      }
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storeQuadrupleCombinationMatchups(matchupData) {
+  try {
+    for (let i = 0; i < 7; i++) {
+      for (let j = i + 1; j < 8; j++) {
+        for (let k = j + 1; k < 9; k++) {
+          for (let l = k + 1; l < 10; l++) {
+            const positions = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+            positions[i] = 0;
+            positions[j] = 0;
+            positions[k] = 0;
+            positions[l] = 0;
+            const alteredMatchupData = getResetMatchupData(
+              matchupData,
+              positions
+            );
+            await storeMatch(alteredMatchupData);
+          }
+        }
+      }
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storePentaCombinationMatchups(matchupData) {
+  try {
+    for (let i = 0; i < 6; i++) {
+      for (let j = i + 1; j < 7; j++) {
+        for (let k = j + 1; k < 8; k++) {
+          for (let l = k + 1; l < 9; l++) {
+            for (let p = l + 1; p < 10; p++) {
+              const positions = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+              positions[i] = 0;
+              positions[j] = 0;
+              positions[k] = 0;
+              positions[l] = 0;
+              positions[p] = 0;
+              const alteredMatchupData = getResetMatchupData(
+                matchupData,
+                positions
+              );
+              await storeMatch(alteredMatchupData);
+            }
+          }
+        }
+      }
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storeHexaCombinationMatchups(matchupData) {
+  try {
+    for (let i = 0; i < 7; i++) {
+      for (let j = i + 1; j < 8; j++) {
+        for (let k = j + 1; k < 9; k++) {
+          for (let l = k + 1; l < 10; l++) {
+            const positions = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            positions[i] = 1;
+            positions[j] = 1;
+            positions[k] = 1;
+            positions[l] = 1;
+            const alteredMatchupData = getResetMatchupData(
+              matchupData,
+              positions
+            );
+            await storeMatch(alteredMatchupData);
+          }
+        }
+      }
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storeHeptaCombinationMatchups(matchupData) {
+  try {
+    for (let i = 0; i < 8; i++) {
+      for (let j = i + 1; j < 9; j++) {
+        for (let k = j + 1; k < 10; k++) {
+          const positions = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+          positions[i] = 1;
+          positions[j] = 1;
+          positions[k] = 1;
+          const alteredMatchupData = getResetMatchupData(
+            matchupData,
+            positions
+          );
+          await storeMatch(alteredMatchupData);
+        }
+      }
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storeOctaCombinationMatchups(matchupData) {
+  try {
+    for (let i = 0; i < 9; i++) {
+      for (let j = i + 1; j < 10; j++) {
+        const positions = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        positions[i] = 1;
+        positions[j] = 1;
+        const alteredMatchupData = getResetMatchupData(matchupData, positions);
+        await storeMatch(alteredMatchupData);
+      }
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storeNonaCombinationMatchups(matchupData) {
+  try {
+    for (let i = 0; i < 10; i++) {
+      const positions = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      positions[i] = 1;
+      const alteredMatchupData = getResetMatchupData(matchupData, positions);
+      await storeMatch(alteredMatchupData);
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storeDecaCombinationMatchups(matchupData) {
+  try {
+    await storeMatch(matchupData);
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function storeMatchup(matchupData) {
+  try {
+    await storeSingleCombinationMatchups(matchupData);
+    await storeDoubleCombinationMatchups(matchupData);
+    await storeTripleCombinationMatchups(matchupData);
+    await storeQuadrupleCombinationMatchups(matchupData);
+    await storePentaCombinationMatchups(matchupData);
+    await storeHexaCombinationMatchups(matchupData);
+    await storeHeptaCombinationMatchups(matchupData);
+    await storeOctaCombinationMatchups(matchupData);
+    await storeNonaCombinationMatchups(matchupData);
+    await storeDecaCombinationMatchups(matchupData);
+  } catch (e) {
+    throw e;
+  }
+}
+
+module.exports = { storeMatchup };
