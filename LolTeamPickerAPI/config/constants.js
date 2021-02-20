6e7c198232c7f2887fcda6ad02571ac574e27f8f
ignjas <ignas.jasonas@gmail.com>
2021-02-21 01:32:21 +0200

    Update gamers IGs

--- LolTeamPickerAPI/config/constants.js
+++ LolTeamPickerAPI/config/constants.js
@@ -3,15 +3,15 @@
 module.exports = {
   servers: {
     BR: { name: "br1", basePlayer: "offlaw2" },
-    EUNE: { name: "eun1", basePlayer: "Nightsheme" },
+    EUNE: { name: "eun1", basePlayer: "RastaMichalSK" },
     EUW: { name: "euw1", basePlayer: "cript1cal" },
-    JP: { name: "jp1", basePlayer: "HIKAKINBOY" },
-    KR: { name: "kr", basePlayer: "32y" },
+    JP: { name: "jp1", basePlayer: "Yogurt" },
+    KR: { name: "kr", basePlayer: "PoruyaOuO" },
     LA1: { name: "la1", basePlayer: "Zherson" },
-    LA2: { name: "la2", basePlayer: "Doublecasted" },
-    NA: { name: "na1", basePlayer: "SandvichTF2" },
-    OCE: { name: "oc1", basePlayer: "PhilFromSchool" },
-    TR: { name: "tr1", basePlayer: "1111111112" },
-    RU: { name: "ru", basePlayer: "SUPRAAAA" },
+    LA2: { name: "la2", basePlayer: "softboy" },
+    NA: { name: "na1", basePlayer: "Chas3" },
+    OCE: { name: "oc1", basePlayer: "Burgotty" },
+    TR: { name: "tr1", basePlayer: "YEDİ" },
+    RU: { name: "ru", basePlayer: "kjge" },
   },
 };
