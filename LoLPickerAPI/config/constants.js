01810ac38b6c674e3de4a81030bd08959ebf0cb0
ignjas <ignas.jasonas@gmail.com>
2021-03-08 01:01:23 +0200

    Update base players for seeding

--- LoLPickerAPI/config/constants.js
+++ LoLPickerAPI/config/constants.js
@@ -2,16 +2,16 @@
 
 module.exports = {
   servers: {
-    BR: { name: "br1", basePlayer: "offlaw2" },
-    EUNE: { name: "eun1", basePlayer: "RastaMichalSK" },
-    EUW: { name: "euw1", basePlayer: "cript1cal" },
-    JP: { name: "jp1", basePlayer: "Yogurt" },
-    KR: { name: "kr", basePlayer: "PoruyaOuO" },
-    LA1: { name: "la1", basePlayer: "Zherson" },
-    LA2: { name: "la2", basePlayer: "softboy" },
-    NA: { name: "na1", basePlayer: "Chas3" },
-    OCE: { name: "oc1", basePlayer: "Burgotty" },
-    TR: { name: "tr1", basePlayer: "YEDİ" },
-    RU: { name: "ru", basePlayer: "kjge" },
+    BR: { name: "br1", basePlayer: "mind set a göd" },
+    EUNE: { name: "eun1", basePlayer: "iKnewSheWas18" },
+    EUW: { name: "euw1", basePlayer: "I BaBa Voss I" },
+    JP: { name: "jp1", basePlayer: "fishpasteman" },
+    KR: { name: "kr", basePlayer: "wo bu zhi dao" },
+    LA1: { name: "la1", basePlayer: "Lê Obscurité" },
+    LA2: { name: "la2", basePlayer: "rodrigovich123" },
+    NA: { name: "na1", basePlayer: "tyler556677" },
+    OCE: { name: "oc1", basePlayer: "Harry Pickle" },
+    TR: { name: "tr1", basePlayer: "Jan Ken Ponn" },
+    RU: { name: "ru", basePlayer: "unkn0wab1e" },
   },
 };
